03aeb292f3540ed5c96c73cf8fec4ccaa2abc2a2
kfkhfj <kfkhfj@gmail.com>
2021-07-23 21:20:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,4 @@
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+echo 'src-git AdGuardHome https://github.com/AdguardTeam/AdGuardHome' >>feeds.conf.default
